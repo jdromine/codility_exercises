27d3ffae21b44a432ba78122dc67e342963976f9
Josh Romine <jromines@gmail.com>
2016-01-03 21:47:07 -0500

    Adding Lesson 3 - Tape Equilibrium

---
+++ Lesson.03.TimeComplexity/Lesson.03.TimeComplexity/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson._03.TimeComplexity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculator.CalculateTapeEquilibrium(new int[] { 3, 1, 2, 4, 3 }));
+            Console.ReadLine();
+        }
+    }
+
+    public class Calculator
+    {
+        public static long? CalculateTapeEquilibrium(int[] N)
+        {
+            long totalSum = N.Sum(i => i); 
+            long? minimumDifference = new long?();
+            long currentDifference;
+            long leftSum = 0;
+            long rightSum = totalSum;
+
+            rightSum = totalSum;
+
+            for (int i = 0; i < N.Length; i++)
+            {
+                leftSum += N[i];
+                rightSum -= N[i];
+                currentDifference = Math.Abs(rightSum - leftSum);
+
+                minimumDifference = minimumDifference == null ? currentDifference : Math.Min((long)minimumDifference, currentDifference);
+            }
+
+            return minimumDifference;
+        }
+    }
+}
