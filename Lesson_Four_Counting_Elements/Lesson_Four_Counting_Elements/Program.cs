ef4b63871cf471216753b936b5ef2b8feba460a2
Josh Romine <jromines@gmail.com>
2016-01-17 11:44:04 -0500

    Adding lesson 4 items 1 & 2

---
+++ Lesson_Four_Counting_Elements/Lesson_Four_Counting_Elements/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_Four_Counting_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.CalculateMissingMinimumPositiveInteger(new int[] { 1, 3, 6, 5, 4, 1, 2 });
+            Calculator.CalculateMinTimeFrogCanCrossRiver(new int[] { 1, 3, 1, 4, 2, 3, 5, 4 }, 5);
+
+            Console.ReadLine();
+        }
+    }
+   
+
+    public class Calculator
+    {
+        public static int? CalculateMissingMinimumPositiveInteger(int[] A)
+        {
+            //Filter out the negatives from the list and order
+            var itemsGreaterThanZero = from item in A
+                    where item > 0
+                    orderby item
+                    select item;
+
+            //Set the minimum expected next element
+            int expectedNextElement = 1;
+
+            //Initialize the min missing element with null
+            int? minMissingElement = null;
+             
+            foreach(var item in itemsGreaterThanZero)
+            {
+                // if the current item == the expected next element, simply increment the expected element
+                if (item == expectedNextElement)
+                {
+                    expectedNextElement++;
+                } else if(item > expectedNextElement) // if the next element is greater than the expected next element, then the missing element has been found.
+                {       
+                    minMissingElement = expectedNextElement;
+                    break;
+                } 
+            }
+
+            return minMissingElement;
+        }
+
+        public static int? CalculateMinTimeFrogCanCrossRiver(int[] A, int X)
+        {
+            Dictionary<int, bool> leaves = new Dictionary<int, bool>();
+
+            int? minTimeToCross = null;
+
+            for (int i = 0; i<A.Length; i++)
+            {
+                //Add the leaves required to cross if they haven't already been added.
+                if (!leaves.ContainsKey(A[i]) && A[i] <= X)
+                {
+                    leaves.Add(A[i], true);
+                }
+
+                //If all leaves have been added, set the min time to cross and break
+                if (leaves.Count == X)
+                {
+                    minTimeToCross = i;
+
+                    break;
+                }
+            }
+
+            return minTimeToCross;
+        }
+    }
+}
---
+++ Lesson_Four_Counting_Elements/Lesson_Four_UnitTests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lesson_Four_Counting_Elements;
+
+namespace Lesson_Four_UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Expect_Missing_Integer_Calculator_To_Return_Five()
+        {
+            Assert.AreEqual(5, Calculator.CalculateMissingMinimumPositiveInteger(new int[] { 1, 3, 6, 4, 1, 2 }));
+        }
+
+        [TestMethod]
+        public void Expect_Negative_Missing_Integer_To_Be_Skipped()
+        {
+            Assert.AreEqual(5, Calculator.CalculateMissingMinimumPositiveInteger(new int[] {-3,-2,0, 1, 3, 6, 4, 1, 2 }));
+        }
+
+        [TestMethod]
+        public void Expect_No_Missing_Integer_To_Be_Null()
+        {
+            Assert.IsNull(Calculator.CalculateMissingMinimumPositiveInteger(new int[] { -3, -2, -1, 0, 1, 3, 6, 5, 4, 1, 2 }));
+        }
+
+        [TestMethod]
+        public void Expect_MinTimeToCross_Calculator_To_Be_Six()
+        {
+            Assert.AreEqual(6, Calculator.CalculateMinTimeFrogCanCrossRiver(new int[] { 1,3,1,4,2,3,5,4}, 5));
+        }
+
+        [TestMethod]
+        public void Expect_MinTimeToCross_Calculator_To_Be_NULL_When_Cant_Cross()
+        {
+            Assert.IsNull(Calculator.CalculateMinTimeFrogCanCrossRiver(new int[] { 1, 3, 1, 4, 2, 3, 2, 4 }, 5));
+        }
+    }
+}
