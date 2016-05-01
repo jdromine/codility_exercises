6d93e0c79d13594c6452c9e53c19c5d8b9274cbe
Josh Romine <jromines@gmail.com>
2016-05-01 14:17:53 -0400

    Cleaning up Binary Gap Computer for Lesson One

---
+++ Lesson.One/Lesson.One.Tests/BinaryGapComputerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lesson.One.Tests
+{
+    [TestClass]
+    public class BinaryGapComputerTests
+    {
+        [TestMethod]
+        public void Expect_15_to_Return_Zero()
+        {
+            Assert.AreEqual(0, BinaryGapComputer.Compute(15));
+        }
+
+        [TestMethod]
+        public void Expect_20_to_Return_One()
+        {
+            Assert.AreEqual(1, BinaryGapComputer.Compute(20));
+        }
+
+        [TestMethod]
+        public void Expect_1041_to_Return_Five()
+        {
+            Assert.AreEqual(5, BinaryGapComputer.Compute(1041));
+        }
+    }
+}
---
+++ Lesson.One/Lesson.One/BinaryGapComputer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson.One
+{
+    public class BinaryGapComputer
+    {
+        public static int Compute(int n)
+        {
+            int maxBinaryGapLength = 0;
+            int currentBinaryGapLength = 0;
+
+            //convert the int to binary
+            var characters = Convert.ToString(n, 2).ToArray();
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                if (characters[i] == '0')
+                {
+                    //Increment the current binary gap
+                    if (currentBinaryGapLength > 0)
+                    {
+                        currentBinaryGapLength++;
+                    }
+                    //Initialize a new binary gap
+                    else
+                    {
+                        if (i > 0 && characters[i - 1] == '1')
+                        {
+                            currentBinaryGapLength = 1;
+                        }
+                    }
+                }
+                //update the max and reset the current binary gap
+                else
+                {
+                    maxBinaryGapLength = Math.Max(maxBinaryGapLength, currentBinaryGapLength);
+                    currentBinaryGapLength = 0;
+                }
+            }
+            return maxBinaryGapLength;
+        }
+    }
+}
